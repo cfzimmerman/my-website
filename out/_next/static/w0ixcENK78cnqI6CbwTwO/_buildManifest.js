85550fc6ae2a5cd0650fab67fbe0c52a4e12f5dd
rendergame <admin@render.game>
2022-10-10 21:31:33 -0500

    add out directory

---
+++ out/_next/static/w0ixcENK78cnqI6CbwTwO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c5f2f512ccc2fa62.css","static/chunks/pages/index-364c815a86f04f10.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
