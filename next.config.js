e11fced691d69cd162ecf32eb58a3371224e4700
rendergame <admin@render.game>
2022-10-10 22:13:33 -0500

    remove out dir

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const repo = "my-website";
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
